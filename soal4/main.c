248655d84ac052b54a2004d46dc363934ad2da18
Bijak <mbijakbestari@gmail.com>
2025-02-25 22:44:59 +0700

    Nomor 5 ngerjain -bijak

--- soal4/main.c
+++ soal4/main.c
@@ -1,5 +1,67 @@
 #include "matrix.h"
 
+/* 
+ * Membuat matrix baru dengan nilai a, b, c, d
+ */
+Matrix newMatrix(int a, int b, int c, int d) {
+    Matrix m;
+    m.a = a;
+    m.b = b;
+    m.c = c;
+    m.d = d;
+    return m;
+}
+
+/* 
+ * Menampilkan matrix ke layar dengan format:
+ * [a b]
+ * [c d]
+ */
+void printMatrix(Matrix m) {
+    printf("[%d %d]\n", m.a, m.b);
+    printf("[%d %d]\n", m.c, m.d);
+}
+
+/* 
+ * Menambahkan dua matrix m1 dan m2
+ */
+Matrix add(Matrix m1, Matrix m2) {
+    Matrix result;
+    result.a = m1.a + m2.a;
+    result.b = m1.b + m2.b;
+    result.c = m1.c + m2.c;
+    result.d = m1.d + m2.d;
+    return result;
+}
+
+/* 
+ * Mengurangkan m2 dari m1
+ */
+Matrix subtract(Matrix m1, Matrix m2) {
+    Matrix result;
+    result.a = m1.a - m2.a;
+    result.b = m1.b - m2.b;
+    result.c = m1.c - m2.c;
+    result.d = m1.d - m2.d;
+    return result;
+}
+
+/* 
+ * Mentranspose matrix m secara in-place
+ */
+void transpose(Matrix *m) {
+    int temp = m->b;
+    m->b = m->c;
+    m->c = temp;
+}
+
+/* 
+ * Menghitung determinan matrix m
+ */
+int determinant(Matrix m) {
+    return m.a * m.d - m.b * m.c;
+}
+
 int main()
 {
 	Matrix m1 = newMatrix(1, 6, 3, 4);
